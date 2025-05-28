6c59c9d59e78d8416c52ee23417ea10d58d85bc9
Liu <liuq13@NIDA-MN02251168.local>
2025-05-28 13:08:32 -0400

    delete old files

--- Scripts_matlab/RunCode.m
+++
@@ -1,9 +0,0 @@
-
-
-home_dir = '/Users/liuq13/Documents/Projects/NODEAP_data_analysis/Scripts_matlab';
-addpath(genpath(home_dir));
-
-run(fullfile(home_dir,'MRI_data','Do_6_post_PAID.m'))
-
-home_dir = '/Users/liuq13/Documents/Projects/NODEAP_data_analysis/Scripts_matlab';
-run(fullfile(home_dir,'FuncConn_MNI','FuncConn_PAID.m'))
